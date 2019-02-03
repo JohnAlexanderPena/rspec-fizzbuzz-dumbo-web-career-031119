5f2df43f89a5163ad4453f6a290cd8a4712f281d
John Pena <jpena91@gmail.com>
2019-02-03 20:29:11 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,10 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz
-  if int % 3 == 0 # if the number int is divisible by 3
+  if int % 3 == 0 
     "Fizz" 
+  elsif int % 5 ==0 
+    "Buzz"
+  elsif int % 15
   end
 end
